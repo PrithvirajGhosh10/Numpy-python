efbf396cbf47fadf739f3164f63d41b8c6e8f826
PrithvirajGhosh10 <aloghosh802@gmail.com>
2026-05-28 23:10:31 +0530

    Learned NumPy array creation and array operations
    Introduction to NumPy arrays, array creation, and basic operations in Python.

---
+++ 02_list.py
@@ -0,0 +1,7 @@
+import numpy as np
+a=[1,2,3,4] #printing list
+print(a)
+
+b=[3,4,6,7,10]
+b=b*2
+print(b) #op: [3, 4, 6, 7, 10, 3, 4, 6, 7, 10]
---
+++ 03_array.py
@@ -0,0 +1,6 @@
+import numpy as np
+array=np.array([1,2,3,4])
+print(array)
+print(type(array)) # op: <class 'numpy.ndarray'>
+a=array*2
+print(a)
